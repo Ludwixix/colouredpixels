a26e1b8c1f935ffee41cb1d3467999e0906af211
Sam Ludwig <sam.ludwig@engagesq.com>
2021-08-31 13:22:50 +1000

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [userInput, setUserInput] = useState()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='topWrapper'>
+      <div onClick={() => setUserInput(userInput ? false: true)} className='wrapper'>
+        {builder()}
+      </div>
     </div>
   );
+}
+
+function builder(){
+  return buildUniqueColoursArray().map((x,index) => {
+     return <div
+      className='pixelItem'
+      style = {{backgroundColor: x.value}}>  
+      </div>
+      }
+  )}
+
+function getRandomColour(min, max){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const x = Math.floor(Math.random() * (max - min + 1) + min)
+  const y = Math.floor(Math.random() * (max - min + 1) + min)
+  const z = Math.floor(Math.random() * (max - min + 1) + min)
+  var rgb = `rgb(${x},${y},${z})`
+ return rgb
+}
+
+function buildUniqueColoursArray() {
+  var count = 0;
+  var map = new Map();
+  var doopsCount = 0;
+  while(count < 32768){
+    var newCol = false;
+    while( newCol === false){
+      const newColor = getRandomColour(0,256)
+      // if color exists in map get a new one until it doesn't
+      if(!map.has(newColor)){
+        map.set(newColor, newColor);
+        newCol = true;
+      } else {
+      // count duplicate colours found 
+        doopsCount ++
+      }
+    }
+  count = count +1
+}
+// map to arr
+   const mapArray = Array.from( map ).map(([key, value]) => ({ key, value }));
+// return arr
+  return mapArray
 }
 
 export default App;
